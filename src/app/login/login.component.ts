1bee7286d0bb72e45ec56cd97c65f088bccdd1f4
sudanamin <sudanamin@hotmail.com>
2018-01-07 22:58:33 +0400

    add new bg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,8 +29,8 @@ export class LoginComponent implements OnInit {
 
   ngAfterViewInit() {
   //  document.querySelector('body').classList.add('blue');
-    document.body.style.backgroundImage = "url(../../assets/stickies-background.jpg)";
-    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundImage = "url(../../assets/post-it-note.jpg)";
+  //  document.body.style.backgroundRepeat = "no-repeat";
  //   document.body.style.backgroundPosition = "center"; 
 
 }
