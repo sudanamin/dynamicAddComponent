4b4494e353924621b1eb4906849ba73c8d376027
Amin <sudanamin@hotmail.com>
2018-01-24 10:22:29 +0400

    add email var to fix reset pssword bug

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 export class ResetPasswordComponent implements OnInit {
 
   error:any;
+  email:any;
+
   
   constructor(public auth: AuthService,private router: Router) {}
 
